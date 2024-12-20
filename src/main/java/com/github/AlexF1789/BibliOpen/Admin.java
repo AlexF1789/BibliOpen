d72cb90b8d3b26a2a40135dae281ff52729de37c
AlexF1789 <alexnyflora@gmail.com>
2024-12-20 23:24:47 +0100

    sessioni code

--- src/main/java/com/github/AlexF1789/BibliOpen/Admin.java
+++ src/main/java/com/github/AlexF1789/BibliOpen/Admin.java
@@ -2,8 +2,8 @@ package com.github.AlexF1789.BibliOpen;
 
 public class Admin extends Utente {
 
-    public Admin(String cognome, String nome, int ID) {
-        super(cognome, nome, ID);
+    public Admin(String cognome, String nome, String telefono, int ID) {
+        super(cognome, nome, telefono, ID);
     }
 
 }
--- src/main/java/com/github/AlexF1789/BibliOpen/Server.java
+++ src/main/java/com/github/AlexF1789/BibliOpen/Server.java
@@ -1,10 +1,16 @@
 package com.github.AlexF1789.BibliOpen;
 
+import org.apache.commons.codec.digest.DigestUtils;
+import org.json.JSONArray;
 import org.json.JSONObject;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
 
 @RestController
 public class Server {
@@ -26,5 +32,86 @@ public class Server {
 
         return risposta.toString();
     }
+    
+    @RequestMapping(value="/login", method=RequestMethod.GET, produces="application/json")
+    public String performLogin(@RequestParam(name="data") String data, HttpServletRequest richiesta) {
+    	JSONObject risposta = new JSONObject();
+    	JSONObject parametri = new JSONObject(data);
+    	HttpSession sessione = richiesta.getSession();
+    	Utente utente;
+    	
+    	try {
+    		// preleviamo dal JSON parametri i due parametri mail e password
+    		String mail = parametri.getString("mail"), password = parametri.getString("password");
+    		
+    		// criptiamo la password con l'algoritmo di hash SHA256
+			password = DigestUtils.sha256Hex(password);
+    		
+    		// verifichiamo che l'utente esista
+    		Database db = new Database();
+    		utente = db.getUtente(mail, password);
+    		
+    		if(utente != null) {
+    			// salviamo in sessione l'istanza della classe utente
+    			Sessione.creaSessione(sessione, utente);
+    			
+    			if(Sessione.isValida(sessione))
+    				risposta.put("esito", true);
+    			else {
+    				risposta.put("esito", false);
+    				risposta.put("message", "ERR_SESSIONE");
+    			}
+    			
+    		} else {
+    			risposta.put("esito", false);
+    			risposta.put("message", "NOTAUTH");
+    		}
+    		
+    	} catch(Exception e) {
+    		risposta.put("esito", false);
+    		risposta.put("message", e.getMessage());
+    	}
+    	
+    	return risposta.toString();
+    }
+
+	@RequestMapping(value="/logout", method=RequestMethod.GET, produces="application/json")
+	public String performLogout(HttpServletRequest richiesta) {
+		JSONObject risposta = new JSONObject();
+
+		Sessione.terminaSessione(richiesta.getSession());
+		risposta.put("esito", Sessione.isValida(richiesta.getSession()));
+
+		return risposta.toString();
+	}
+
+	@RequestMapping(value="/getSessione", method=RequestMethod.GET, produces="application/json")
+	public String getSessione(HttpServletRequest richiesta) {
+		JSONObject risposta = new JSONObject();
+		HttpSession sessione = richiesta.getSession();
+
+		if(Sessione.isValida(sessione)) {
+			JSONArray dati = new JSONArray();
+			Utente utente = (Utente) sessione.getAttribute("utente");
+
+			if(utente != null) {
+
+				risposta.put("esito", true);
+
+				dati.put(new JSONObject().put("ID", utente.getID()));
+				dati.put(new JSONObject().put("cognome", utente.getCognome()));
+				dati.put(new JSONObject().put("nome", utente.getNome()));
+				dati.put(new JSONObject().put("telefono", utente.getTelefono()));
+
+				risposta.put("data", dati);
+
+			} else
+				risposta.put("esito", false);
+
+		} else
+			risposta.put("esito", false);
+
+		return risposta.toString();
+	}
 
 }
